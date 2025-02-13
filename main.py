649c4b666b23eab0a8bdd08c9246d00db57667b4
thijspoon28 <thijspoon28@gmail.com>
2025-02-14 00:28:12 +0100

    added notebooks

--- main.py
+++ main.py
@@ -5,11 +5,31 @@ from api.sumo import SumoAPI
 def main():
     api = SumoAPI()
 
-    rikishis = api.get_rikishis()
-    rikishi_id = rikishis.records[0].id
+    # rikishis = api.get_rikishis()
+    # print(len(rikishis.records))
+    # active_Rikishi = []
+    # for i in rikishis.records:
+    #     active_Rikishi.append(i.shikonaEn)
+    # print(active_Rikishi)
 
-    print(rikishi_id)
+    # rikishi_id = rikishis.records[0].id
 
+    # print(rikishi_id)
+
+    # rikishis_stats = api.get_rikishi_stats(218)
+    # print(rikishis_stats)
+
+    # kimarite = api.get_kimarite()
+    # print(kimarite)
+    # print(len(kimarite.records))
+
+    kimarite_detail = api.get_kimarite_detail('oshidashi')
+    print(kimarite_detail.records[10])
+    print(kimarite_detail.total)
+    print(len(kimarite_detail.records))
+
+
+    
 
 if __name__ == "__main__":
     main()
