09da2ad85558eb199691765e99eb6424ddbdfd32
pikasven <sven.ellers@gmail.com>
2025-02-25 00:13:08 +0100

    start frontend

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="wrapper">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/basho">Basho</RouterLink>
+      <RouterLink to="">Current basho</RouterLink>
+    </nav>
+</template>
+
+<style>
+.wrapper {
+  background-color: #d8c7b2;
+  display: flex;
+  justify-content: space-around;
+}
+
+.wrapper * {
+  padding: 2%;
+}
+
+.wrapper *:hover {
+  background-color: #4d4740;
+  border-radius: 5px;
+}
+</style>
